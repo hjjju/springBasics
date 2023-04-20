936725f0dcc75f3fc8abf7c456628641edc09451
yuju <yujjuu22@gmail.com>
2023-04-20 21:54:03 +0900

    mvc와 템플릿엔진

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,13 +3,20 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
     @GetMapping("hello") // 웹어플리케이션에서 /hello라고 치면 이 메서드를 호출해줌
     public String hello(Model model){
-        model.addAttribute("data","spring!!");
+        model.addAttribute("data","hello!!");
         return "hello";  //resources/templates/hello.html을 찾아서 랜더링 해라(hello.html을 실행시켜라)
+
+    }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){ //모델에 담으면 뷰에서 랜더링할때 씀
+        model.addAttribute("name",name); // 키,네임
+        return "hello-template";
 
     }
 }
